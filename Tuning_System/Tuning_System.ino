83713d718cb07d13ce4d405ed9ebf70f12acbe31
Ryan Bui <buihuyryan@gmail.com>
2023-07-17 10:53:08 -0400

    Implement autocorrelation algorithm with piano C4 file

--- Tuning_System/Tuning_System.ino
+++ Tuning_System/Tuning_System.ino
@@ -1,47 +1,69 @@
 #include <math.h>
-#include "yinacf.h"
+#include "C4.h"
 
 #define LENGTH 512
 
-YinACF<float> yin;
 float minFreq = 60.0;
-const float sample_freq = 8919;
+const float sample_freq = 22050;
 
-short rawData[LENGTH];
+// short rawData[LENGTH];
+int len = sizeof(rawData);
+int count;
+int i, k;
+long sum, sum_old;
+int thresh = 0;
+float freq_per = 0;
+short pd_state = 0;
 
-void initYin(float sampleRate, float minFreq) {
-	unsigned w, tmax;
-	w = (unsigned)ceil(sampleRate/minFreq);
-	tmax = w;
-	yin.build(w, tmax);
+void measureFrequency() {
+	sum = 0;
+	pd_state = 0;
+	int period = 0;
+	for(i = 0; i < len; i++) {
+		sum_old = sum;
+		sum = 0;
+		for(k = 0; k < len-i; k++) sum += (rawData[k]-128)*(rawData[k+i]-128)/256;
 
-}
+		// Serial.println(sum);
 
-int getFundamentalFrequency(int n, float* inSamples, float* outFrequencies) {
-	int i;
+		if(pd_state == 2 && (sum-sum_old) <= 0){
+			period = i;
+			pd_state = 3;
+		}
 
-	for(i = 0; i < n; ++i) {
-		outFrequencies[i] = yin.tick(inSamples[i]);
+		if(pd_state == 1 && (sum > thresh) && (sum-sum_old) > 0) pd_state = 2;
+
+		if(!i) {
+			thresh = sum * 0.5;
+			pd_state = 1;
+		}
+
+		// for(i=0; i < len; i++) Serial.println(rawData[i]);
+
+		if(thresh > 100){
+			freq_per = sample_freq/period;
+			Serial.println(freq_per);
+		}
+		count = 0;
 	}
-	
-	return 0;
 }
 
 void setup() {
-	analogReference(EXTERNAL);
+	// analogReference(EXTERNAL);
 	// TODO: May need to change this depending on what the ADC pin is on ESP32
-	analogRead(A0);
+	// analogRead(A0);
 	Serial.begin(115200);
+	delay(1000);
 	count = 0;
+
+	measureFrequency();
 }
 
 void loop () {
 	
-	if(count < LENGTH) {
-		count++;
-		rawData[count] = analogRead(A0)>>2;
-	} else {
-		sum = 0;
-
-	}
+	// if(count < LENGTH) {
+	// 	count++;
+	// 	rawData[count] = analogRead(A0)>>2;
+	// } else {
+	// }
 }
