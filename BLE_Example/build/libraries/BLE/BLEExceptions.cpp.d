0af4db50ac1548cb4e48db3185c8be8f50fd9d38
Ryan Bui <buihuyryan@gmail.com>
2023-11-11 15:40:22 -0500

    Remove extra arduino sketches

--- BLE_Example/build/libraries/BLE/BLEExceptions.cpp.d
+++
@@ -1,2 +0,0 @@
-/Users/workryan/Documents/Arduino/BLE_Example/build/libraries/BLE/BLEExceptions.cpp.o: \
- /Users/workryan/Library/Arduino15/packages/esp32/hardware/esp32/2.0.9/libraries/BLE/src/BLEExceptions.cpp
