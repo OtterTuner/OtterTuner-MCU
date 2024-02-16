0f38a198552377c394f2a7311b54bd7e50f8a89a
Ryan Bui <buihuyryan@gmail.com>
2024-02-15 23:31:18 -0500

    Remove retry loop

--- Final/demo/Autocorrelation.ino
+++ Final/demo/Autocorrelation.ino
@@ -1,11 +1,8 @@
-#define LENGTH 512
-
 extern double tunings[6];
 extern int string_number;
 extern short rawData[LENGTH];
 extern double desired_freq;
 
-const float sample_freq = 16667;
 int len = LENGTH;
 int thresh = 0;
 double freq_thres = 0.20 * desired_freq;
@@ -33,7 +30,6 @@ void parseTuningString(String fullTuning) {
             //skip the comma and the space
             i += 1;
             tunings[tuningIndex] = tuning.toDouble();
-            // Serial.printf("tuning at %d: %f\r\n", tuningIndex, tunings[tuningIndex]);
             tuningIndex++;
             tuning = "";
             continue;
@@ -43,55 +39,71 @@ void parseTuningString(String fullTuning) {
     }
 }
 
-int stringCheck(double frequency){
-    if(frequency>70 && frequency<90){ return 0; }
-    if(frequency>100 && frequency<120){ return 1; }
-    if(frequency>135 && frequency<155){ return 2; }
-    if(frequency>186 && frequency<205){ return 3; }
-    if(frequency>235 && frequency<255){ return 4; }
-    if(frequency>320 && frequency<340){ return 5; }
+double avg_buffer(){
+    double sum = 0;
+    for(int i = 0; i < len; i++) sum += rawData[i];
+    return sum/len;
+}
+
+double max_buffer(){
+    double max = 0;
+    for(int i = 0; i < len; i++) if(rawData[i] > max) max = rawData[i];
+    return max;
+}
+
+double min_buffer(){
+    double min = 4096;
+    for(int i = 0; i < len; i++) if(rawData[i] < min) min = rawData[i];
+    return min;
 }
 
 /*
 * 	TUNING SENSING
 */
-double measureFrequency() {
+double measureFrequency(double sample_freq) {
     int sum = 0;
     static int sum_old = 0;
     static short pd_state = 0;
     int period = 0;
     double measured_freq;
+    double max_val = max_buffer();
+    double min_val = min_buffer();
+    double offset = (max_val + min_val) / 2;
+    double normalize = (max_val - min_val);
 
-    for(int attempts = 0; attempts < 32; attempts++){
-        for(int i = 0; i < len; i++) {
-            sum_old = sum;
-            sum = 0;
+    // Serial.printf("Max: %f, Min: %f, Offset: %f, Normalize: %f\r\n", max_val, min_val, offset, normalize);
 
-            for(int k = 0; k < len-i; k++) sum += (rawData[k]-2048)*(rawData[k+i]-2048)/4096;
+    for(int i = 0; i < len; i++) {
+        sum_old = sum;
+        sum = 0;
 
-            if(pd_state == 2 && (sum-sum_old) <= 0){
-                period = i;
-                // Serial.printf("period: %d\n", period);
-                pd_state = 3;
-            }
+        for(int k = 0; k < len-i; k++) sum += (rawData[k]-2048)*(rawData[k+i]-2048)/4096;
 
-            if(pd_state == 1 && (sum > thresh) && (sum-sum_old) > 0) pd_state = 2;
-
-            if(!i) {
-                thresh = sum * 0.5;
-                pd_state = 1;
-            }
+        if(pd_state == 2 && (sum-sum_old) <= 0){
+            period = i;
+            pd_state = 3;
         }
 
-        if(period != 0 && thresh > 70) {
-            // offset by 4 because we're bad lol
-            measured_freq = sample_freq/period - 4;
-            double discrepancy = abs(desired_freq - measured_freq);
+        if(pd_state == 1 && (sum > thresh) && (sum-sum_old) > 0) pd_state = 2;
 
-            if(discrepancy < freq_thres) {
-                Serial.printf("measured_frequency: %f\r\n", measured_freq);
-                return measured_freq;
-            }
+        if(!i) {
+            thresh = sum * 0.5;
+            pd_state = 1;
+        }
+    }
+
+    if(period != 0) {
+        measured_freq = sample_freq/period;
+        freq_thres = 0.20 * desired_freq;
+        double avg = avg_buffer();
+        // Serial.printf("Average: %f\n", avg);
+        Serial.printf("Measured frequency: %f, sample_frequency: %f\r\n", measured_freq, sample_freq);
+
+        double discrepancy = abs(desired_freq - measured_freq);
+
+        if(discrepancy < freq_thres) {
+            // Serial.printf("Desired frequency: %f\n", desired_freq);
+            return measured_freq;
         }
     }
 
--- Final/demo/demo.ino
+++ Final/demo/demo.ino
@@ -1,14 +1,16 @@
 #include <math.h>
 #include <string>
 #include <Preferences.h>
+#include <driver/adc.h>
 
 #define DEVICE_NAME "OtterTuner"
-#define LENGTH 512
+#define LENGTH 4000
 
 Preferences preferences;
-double tunings[6] = {0};
-int string_number = 0;
-double desired_freq = tunings[string_number];
+double tunings[6] = {82.41, 110.00, 146.83, 196.00, 246.94, 329.63};
+int string_number;
+double desired_freq;
+double sample_freq;
 int count = 0;
 short rawData[LENGTH];
 
@@ -21,29 +23,66 @@ double get_tuning(){
 	return tunings[string_number];
 }
 
+void adc_setup(){
+    adc_digi_init_config_t config;
+	config.max_store_buf_size = 1024;
+	config.adc1_chan_mask = BIT(6);
+	config.adc2_chan_mask = 0;
+	config.conv_num_each_intr = 256;
+
+	adc_digi_pattern_config_t adc_pattern;
+	adc_pattern.atten = ADC_ATTEN_DB_0;
+	adc_pattern.channel = ADC1_CHANNEL_6;
+	adc_pattern.unit = ADC_UNIT_1;
+	adc_pattern.bit_width = 12;
+
+    adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_DB_0);
+
+	adc_digi_configuration_t controller_config;
+	controller_config.conv_limit_en = 0;
+	controller_config.conv_limit_num = 250;
+	controller_config.pattern_num = 1;
+	controller_config.adc_pattern = &adc_pattern;
+	controller_config.sample_freq_hz = 60000;
+	controller_config.conv_mode = ADC_CONV_SINGLE_UNIT_1;
+	controller_config.format = ADC_DIGI_OUTPUT_FORMAT_TYPE2;
+}
+
 void setup() {
 	Serial.begin(115200);
 	motorSetup();
 	bluetooth_init();
+    adc_setup();
+    sample_freq = 0;
 	delay(3000);
 	Serial.println("setup complete");
 }
 
 void loop() {
-	Serial_Monitor();
+	// Serial_Monitor();
+    int in = Serial.read();
+    if (in == 'n') {
+        string_number = (string_number + 1) % 6;
+        desired_freq = tunings[string_number];
+        Serial.printf("string number: %d\r\n", string_number);
+    } else if (in == 'p') {
+        string_number--;
+        desired_freq = tunings[string_number];
+        Serial.printf("string number: %d\r\n", string_number);
+    }
 
-	if(count < LENGTH) {
-		rawData[count] = analogRead(A0);
-		count++;
-	} else {
-		double current_frequency = measureFrequency();
-		string_number = stringCheck(current_frequency);
+    double startTime = millis();
+    for (int i = 0; i < LENGTH; i++) {
+        rawData[i] = adc1_get_raw(ADC1_CHANNEL_0);
+    }
+    double endTime = millis();
+    sample_freq = (LENGTH / (endTime - startTime)) * 1000;
 
-		double prev_freq = desired_freq;
-		desired_freq = get_tuning();
-		Serial.printf("desired freq: %f\r\n", desired_freq);
+    double prev_freq = desired_freq;
 
-		pid(current_frequency);
-		count = 0;
-	}
+    double current_frequency = measureFrequency(sample_freq);
+    // Serial.printf("desired freq: %f\r\n", desired_freq);
+
+    pid(current_frequency);
+    count = 0;
 }
