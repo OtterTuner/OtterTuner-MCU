f9857f2ededec20d912cd6bd2de6d79742296d6b
Ryan Bui <buihuyryan@gmail.com>
2024-02-28 16:46:52 -0500

    Add debounce time of 100ms

--- mcu/Button.ino
+++ mcu/Button.ino
@@ -1,14 +1,22 @@
-#define BUTTON_PIN 12
+#define BUTTON_PIN      12
+#define DEBOUNCE_TIME   100
+
+int last_interrupt_time = millis();
 
 void IRAM_ATTR ISR() {
     Serial.println("Button pressed");
 
-    string_number = (string_number + 1) % 6;
-    desired_freq = tunings[string_number];
-    Serial.printf("string number: %d\r\n", string_number);
+    int interrupt_time = millis();
+    if ( interrupt_time - last_interrupt_time > DEBOUNCE_TIME ) {
+        string_number = ( string_number + 1 ) % 6;
+        Serial.printf("string number: %d\r\n", string_number);
+
+        desired_freq = tunings[string_number];
+        last_interrupt_time = interrupt_time;
+    }
 }
 
 void buttonSetup() {
-    pinMode(BUTTON_PIN, INPUT);
-    attachInterrupt(BUTTON_PIN, ISR, RISING);
+    pinMode( BUTTON_PIN, INPUT );
+    attachInterrupt( BUTTON_PIN, ISR, RISING );
 }
