690263c62eff934ac2b3542ca5f2053f8d94b6db
Ryan Bui <buihuyryan@gmail.com>
2024-03-02 18:38:50 -0500

    Attempt to implement count of values

--- mcu/Button.ino
+++ mcu/Button.ino
@@ -1,11 +1,12 @@
-#define STRING_SWITCH_PIN   9
+#define BUTTON_PIN          7
 #define DEBOUNCE_TIME       100
+#define MIN_PRESS_TIME      200
 #define HOLD_THRESHOLD      1000
 
 volatile int pressed_time = 0;
 volatile int last_interrupt_time = 0;
 
-void IRAM_ATTR ISR() {
+void IRAM_ATTR ISR_Button() {
     int time = millis();
     // Serial.printf("time: %d, last_interrupt_time: %d\r\n", time, last_interrupt_time);
 
@@ -17,12 +18,12 @@ void IRAM_ATTR ISR() {
             Serial.println("Button pressed");
         } else {
             Serial.println("Button Released");
-            if( time - pressed_time < HOLD_THRESHOLD ) {
+            if( time - pressed_time < HOLD_THRESHOLD && time - pressed_time > MIN_PRESS_TIME ) {
                 string_number = ( string_number + 1 ) % 6;
                 Serial.printf("string number: %d\r\n", string_number);
 
                 desired_freq = tunings[string_number];
-            } else {
+            } else if( time - pressed_time > MIN_PRESS_TIME ) {
                 Serial.println("Entering Bluetooth mode");
                 g_centralConnected = false;
                 BLEDevice::startAdvertising();
@@ -33,8 +34,19 @@ void IRAM_ATTR ISR() {
     }
 }
 
+void IRAM_ATTR ISR2() {
+    int buttonRead = digitalRead(digitalPinToInterrupt(BUTTON_PIN));
+
+    if(buttonRead == HIGH) {
+        Serial.println("Button pressed");
+    } else {
+        Serial.println("Button Released");
+    }
+}
+
 void buttonSetup() {
     pinMode( STRING_SWITCH_PIN, INPUT_PULLDOWN );
     pinMode( TUNING_BUTTON_PIN, INPUT_PULLDOWN );
-    attachInterrupt( digitalPinToInterrupt(STRING_SWITCH_PIN), ISR, CHANGE );
+    attachInterrupt( digitalPinToInterrupt(STRING_SWITCH_PIN), ISR_Button, CHANGE );
+    attachInterrupt( digitalPinToInterrupt(BUTTON_PIN), ISR2, CHANGE);
 }
--- mcu/mcu.ino
+++ mcu/mcu.ino
@@ -5,12 +5,29 @@
 
 #define DEVICE_NAME "OtterTuner"
 #define LENGTH 4000
-#define TUNING_BUTTON_PIN 8
+#define TUNING_BUTTON_PIN   8
+#define STRING_SWITCH_PIN   9
+#define STABLE_THRESHOLD    10
 
 Preferences preferences;
 double desired_freq;
 double sample_freq;
 short rawData[LENGTH];
+volatile bool buttonInterrupt;
+int no_bounce_count = 0;
+bool interrupt_disabled = false;
+int previous_button_value = LOW;
+
+void IRAM_ATTR ISR() {
+    // set interrupt flag and then exit
+    // disable interrupts
+    // check for stable value
+    // do something
+    // then reenable interrupt
+
+    Serial.println("Setting button interrupt");
+    buttonInterrupt = true;
+}
 
 /*
  * Indices for string numbers are as follows:
@@ -30,6 +47,15 @@ double get_tuning(){
 	return tunings[string_number];
 }
 
+double count_bounce(int buttonRead ) {
+    Serial.printf("counting bounce: %d \r\n", no_bounce_count);
+    if (buttonRead != previous_button_value) {
+        no_bounce_count = 0; 
+    } else {
+        no_bounce_count++;
+    }
+}
+
 void setup_batteryADC() {
 
 }
@@ -39,7 +65,11 @@ void setup() {
     motorSetup();
     bluetooth_init();
     adc_setup();
-    buttonSetup();
+    // buttonSetup();
+
+    pinMode( TUNING_BUTTON_PIN, INPUT_PULLDOWN );
+    attachInterrupt( digitalPinToInterrupt(STRING_SWITCH_PIN), ISR, CHANGE );
+
     sample_freq = 0;
     delay(3000);
     Serial.println("setup complete");
@@ -49,7 +79,29 @@ void loop() {
 	// Serial_Monitor();
     int isTuningOn = digitalRead( TUNING_BUTTON_PIN );
     
-    if( isTuningOn == HIGH ) {
+    if( buttonInterrupt ){
+        Serial.println("Found interrupt");
+        // if (!interrupt_disabled){
+        noInterrupts();
+        //     interrupt_disabled = true;
+        // }
+        int buttonRead = digitalRead(digitalPinToInterrupt(STRING_SWITCH_PIN));
+
+        count_bounce(buttonRead);
+
+        if (no_bounce_count > STABLE_THRESHOLD){
+            if (buttonRead == HIGH) {
+                Serial.println("Button pressed");
+            } else if (buttonRead == LOW) {
+                Serial.println("Button released");
+            }
+
+            buttonInterrupt = false;
+            // interrupt_disabled = false;
+            interrupts();
+        }
+
+    } else if( isTuningOn == HIGH ) {
         double startTime = millis();
         for (int i = 0; i < LENGTH; i++) {
             rawData[i] = adc1_get_raw(ADC1_CHANNEL_4);
@@ -66,7 +118,7 @@ void loop() {
         pid(current_frequency);
     } else {
         // TODO: Insert battery reading code
-        int batteryVoltage = analogRead(ADC1_CHANNEL_9);
-        Serial.printf("Battery Voltage: %d\r\n", batteryVoltage);
+        int batteryVoltage = analogRead(ADC1_CHANNEL_MAX);
+        // Serial.printf("Battery Voltage: %d\r\n", batteryVoltage);
     }
 }
