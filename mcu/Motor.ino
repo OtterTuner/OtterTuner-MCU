18a1cff60a0d0b2e8f07b333c79521de63a41f00
Ryan Bui <buihuyryan@gmail.com>
2024-03-03 11:40:54 -0500

    Add PWM functionality to Motors

--- mcu/Motor.ino
+++ mcu/Motor.ino
@@ -1,3 +1,5 @@
+#define PWM_FREQUENCY   30000
+#define PWM_MAX_VALUE   255
 /*
 * Motor globals
 */
@@ -25,38 +27,41 @@ const int resolution = 8;
 *	MOTOR CONTROL
 */
 
+void softwarePWM(int pin, int dutyCycle) {
+    int onTime = map(dutyCycle, 0, PWM_MAX_VALUE, 0, 100) * PWM_FREQUENCY / 100;
+    int offTime = PWM_FREQUENCY - onTime;
+
+    digitalWrite(pin, HIGH);
+    delay(onTime);
+
+    digitalWrite(pin, LOW);
+    delay(offTime);
+}
+
 void motorSetup() {
-  // sets the pins as outputs:
-  pinMode(motor1Pin1, OUTPUT);
-  pinMode(motor1Pin2, OUTPUT);
-  pinMode(enable1Pin, OUTPUT);
-
-  // configure LED PWM functionalitites
-  ledcSetup(pwmChannel, freq, resolution);
-
-  // attach the channel to the GPIO to be controlled
-  ledcAttachPin(enable1Pin, pwmChannel);
+    // sets the pins as outputs:
+    pinMode(motor1Pin1, OUTPUT);
+    pinMode(motor1Pin2, OUTPUT);
+    pinMode(enable1Pin, OUTPUT);
 }
 
 void unwindString(){
-    setMotor(-1, 256, NULL, motor1Pin1, motor1Pin2);
+    setMotor(-1, PWM_MAX_VALUE, NULL, motor1Pin1, motor1Pin2);
 }
 
 void setMotor(int dir, int pwmVal, int pwm, int in1, int in2){
-  ledcWrite(pwmChannel, pwmVal);
-
-  if(dir == 1){
-	digitalWrite(in1,HIGH);
-	digitalWrite(in2,LOW);
-  }
-  else if(dir == -1){
-	digitalWrite(in1,LOW);
-	digitalWrite(in2,HIGH);
-  }
-  else{
-	digitalWrite(in1,LOW);
-	digitalWrite(in2,LOW);
-  }
+    if(dir == 1){
+        softwarePWM(in1, pwmVal);
+        digitalWrite(in2,LOW);
+    }
+    else if(dir == -1){
+        digitalWrite(in1,LOW);
+        softwarePWM(in2, pwmVal);
+    }
+    else{
+        digitalWrite(in1,LOW);
+        digitalWrite(in2,LOW);
+    }
 }
 
 
