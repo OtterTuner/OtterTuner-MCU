336683df137887d588c36e4cd9c7282f289a5fbb
Ryan Bui <buihuyryan@gmail.com>
2024-03-02 19:30:12 -0500

    Add string number change and bluetooth pairing mode

--- mcu/Button.ino
+++ mcu/Button.ino
@@ -10,14 +10,10 @@ void buttonSetup() {
 }
 
 void buttonHandler() {
-    Serial.println("Button interrupt");
     delayMicroseconds(DEBOUNCE_TIME);
     int buttonRead = digitalRead(digitalPinToInterrupt(STRING_SWITCH_PIN));
     Serial.printf("button read: %d\r\n", digitalRead(digitalPinToInterrupt(STRING_SWITCH_PIN)));
-    // for(int i = 0; i < 10; i++) {
-    //     Serial.printf("button read: %d\r\n", digitalRead(digitalPinToInterrupt(STRING_SWITCH_PIN)));
-    // }
-    // do something
+
     if(buttonRead == HIGH) {
         Serial.println("Button pressed");
         pressed_time = millis();
@@ -26,32 +22,12 @@ void buttonHandler() {
         int release_time = millis();
 
         if(release_time - pressed_time > LONG_PRESS ) {
-            Serial.println("Long Press");
+            Serial.println("Entering bluetooth pairing mode");
+            g_centralConnected = false;
+            BLEDevice::startAdvertising();
         } else {
-            Serial.println("Short press");
+            string_number = (string_number + 1) % 6;
+            Serial.printf("New String number: %d\r\n", string_number);
         }
     }
-
-    // if (time - last_interrupt_time > DEBOUNCE_TIME) {
-    //     Serial.println("Button change");
-    //     int buttonRead = digitalRead(digitalPinToInterrupt(STRING_SWITCH_PIN));
-    //     if( buttonRead == HIGH ) {
-    //         // pressed_time = time;
-    //         Serial.println("Button pressed");
-    //     } else {
-    //         Serial.println("Button Released");
-    //         if( time - pressed_time < HOLD_THRESHOLD ) {
-    //             string_number = ( string_number + 1 ) % 6;
-    //             Serial.printf("string number: %d\r\n", string_number);
-
-    //             desired_freq = tunings[string_number];
-    //         } else {
-    //             Serial.println("Entering Bluetooth mode");
-    //             g_centralConnected = false;
-    //             BLEDevice::startAdvertising();
-    //         }
-    //     }
-
-    //     last_interrupt_time = time;
-    // }
 }
