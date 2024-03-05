375b9be8c4b4737e8096bf6a5af1f4a946724988
Ryan Bui <buihuyryan@gmail.com>
2024-03-05 15:33:48 -0500

    Change pins for buttons

--- mcu/LED.ino
+++ mcu/LED.ino
@@ -6,19 +6,15 @@ void LED_Setup(){
 
 void BLEAnimation() {
     digitalWrite(LEDS[0], HIGH);
-    Serial.printf("LED[%d] value: %d\r\n", 0, digitalRead(LEDS[0]));
 
     for(int i = 1; i < NUM_LEDS; i++) {
         digitalWrite(LEDS[i], HIGH);
-        Serial.printf("LED[%d] value: %d\r\n", i, digitalRead(LEDS[i]));
         delay(10);
         digitalWrite(LEDS[i-1], LOW);
-        Serial.printf("LED[%d] value: %d\r\n", i-1, digitalRead(LEDS[i-1]));
         delay(10);
     }
 
     digitalWrite(LEDS[NUM_LEDS-1], LOW);
-    Serial.printf("LED[%d] value: %d\r\n", NUM_LEDS-1, digitalRead(LEDS[NUM_LEDS-1]));
 }
 
 void LowBatteryAnimation() {
--- mcu/mcu.ino
+++ mcu/mcu.ino
@@ -6,10 +6,14 @@
 #define DEVICE_NAME "OtterTuner"
 
 #define LENGTH 2000
-#define MAX_ADC_VALUE       4096
+#define MAX_BATTERY_ADC     1300
+#define MAX_BATTERY_VOLT    4.2
+#define LOW_BATTERY_VOLT    3.5
+#define LOW_BATTERY_PERC    LOW_BATTERY_VOLT / MAX_BATTERY_VOLT
+#define LOW_BATTERY_ADC     LOW_BATTERY_PERC * MAX_BATTERY_ADC
 
-#define STRING_SWITCH_PIN   3
-#define TUNING_BUTTON_PIN   4
+#define STRING_SWITCH_PIN   2
+#define TUNING_BUTTON_PIN   3
 #define VBAT_PIN            10
 
 #define MOTOR_IN1_PIN   7
@@ -26,10 +30,22 @@
 #define LED5_PIN        9
 #define LED6_PIN        8
 
+bool enableSerial = 0;
+
 int LEDS[NUM_LEDS] = {LED1_PIN, LED2_PIN, LED3_PIN, LED4_PIN, LED5_PIN, LED6_PIN};
 
 TaskHandle_t producerTask;
 SemaphoreHandle_t semaphore;
+volatile bool buttonInterrupt = false;
+
+/*
+ * Indices for string numbers are as follows:
+ * E2 A2 D3 G3 B3 E4
+ * 0  1  2  3  4  5
+ */
+
+double tunings[NUM_STRINGS] = {82.41, 110.00, 146.83, 196.00, 246.94, 329.63};
+int string_number;
 
 Preferences preferences;
 double desired_freq;
@@ -44,7 +60,6 @@ void producerTaskCode( void * parameters ) {
         if (semaphore != NULL && xSemaphoreTake( semaphore, (TickType_t) 10) == pdTRUE )
         {
             int cid = xPortGetCoreID();
-            // Serial.printf("Producer task code running on core %d\r\n", cid);
             getSamples();
             current_frequency = measureFrequency(sample_freq);
             Serial.printf("current_frequency: %f, desired freq: %f\r\n", current_frequency, desired_freq);
@@ -53,17 +68,6 @@ void producerTaskCode( void * parameters ) {
         }
     }
 }
-
-/*
- * Indices for string numbers are as follows:
- * E2 A2 D3 G3 B3 E4
- * 0  1  2  3  4  5
- */
-
-double tunings[NUM_STRINGS] = {82.41, 110.00, 146.83, 196.00, 246.94, 329.63};
-int string_number = 0;
-
-volatile bool buttonInterrupt = false;
 
 void IRAM_ATTR ISR() {
     buttonInterrupt = true;
@@ -86,6 +90,8 @@ void setup() {
     buttonSetup();
     LED_Setup();
     sample_freq = 0;
+    string_number = 0;
+    desired_freq = tunings[string_number];
     pinMode( VBAT_PIN, OUTPUT );
 
     semaphore = xSemaphoreCreateMutex();
@@ -103,7 +109,7 @@ void setup() {
 }
 
 void loop() {
-	Serial_Monitor();
+	// Serial_Monitor();
     int isTuningOn = digitalRead( TUNING_BUTTON_PIN );
     
     if( buttonInterrupt )
@@ -113,7 +119,6 @@ void loop() {
     }
     else if ( isTuningOn == HIGH )
     {
-
         // If we're at string number 6, then we're unwinding the string
         if (string_number == UNWIND_MODE)
         {
@@ -134,13 +139,14 @@ void loop() {
     else
     {
         // TODO: Insert battery reading code
-        float batteryVoltage = analogRead(ADC1_CHANNEL_MAX);
-        float batteryPercentage = batteryVoltage / MAX_ADC_VALUE;
+        float batteryVoltage = analogRead(VBAT_PIN);
 
-        if( batteryPercentage <= 0.2)
+        // Serial.printf("battery voltage: %f, low battery threshold: %f, low battery percentage: %f\r\n", batteryVoltage, LOW_BATTERY_ADC, LOW_BATTERY_PERC);
+
+        if( batteryVoltage <= LOW_BATTERY_ADC)
         {
-            Serial.println("Battery low!");
-            LowBatteryAnimation();
+            // Serial.println("Battery low!");
+            // LowBatteryAnimation();
         }
     }
 }
